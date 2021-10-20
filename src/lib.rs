2c51680f628529955bfb7648b0819041b2bd8441
Bao Le <uac6qw@virginia.edu>
2021-10-20 11:55:57 -0400

    Doesn't work

--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,10 @@ use std::{sync::{Arc, Mutex, }, iter};
 use std::iter::Map;
 
 pub struct PeckLEDs {
-    pub light_handles: Vec<LineHandle>,
-    pub ir_handles: Vec<LineHandle>,
+    right_leds: MultiLineHandle,
+    center_leds: MultiLineHandle,
+    left_leds: MultiLineHandle,
+    pub peck_position: Vec<LedState>,
 }
 pub struct PeckKeys {
 }
@@ -23,8 +25,41 @@ pub struct PeckBoard {
     pub leds: PeckLEDs,
     pub keys: PeckKeys,
 }
+pub enum LedState {
+    Off,
+    Red = 1,
+    Blue = 2,
+    Green = 3,
+    All = 4,
+}
+impl LedState {
+
+    fn next(&mut self) -> Self {
+        match self {
+            LedState::Off => {*self = LedState::Red}
+            LedState::Red => {*self = LedState::Blue}
+            LedState::Blue => {*self = LedState::Green}
+            LedState::Green => {*self = LedState::Blue}
+            LedState::All => {*self = LedState::All}
+        };
+        //*self
+        LedState::Red
+    }
+    fn as_value(&self) -> [u8; 3] {
+        match self {
+            LedState::Off => {[0,0,0]}
+            LedState::Red => {[1,0,0]}
+            LedState::Blue => {[0,1,0]}
+            LedState::Green => {[0,0,1]}
+            LedState::All => {[1,1,1]}
+        }
+    }
+}
 
 impl PeckBoard {
+    const INTERRUPT_CHIP: &'static str = "/dev/gpiochip2";
+    const INTERRUPT_LINE: u32 = 24;
+    const PECK_KEY_LINES: [u32; 3] = [13,14,15];
     pub async fn new (chip: &str) -> Result<Self, Error> {
         let mut chip = Chip::new(chip).map_err(|e:GpioError|
             Error::ChipError {source: e,
@@ -37,41 +72,7 @@ impl PeckBoard {
             keys
         })
     }
-}
-impl PeckLEDs {
-    const LINES: [u32;9] = [0,1,2,3,4,5,6,7,8];
-    const IR: [u32; 3] = [9,10,11];
-    pub fn new(chip: &mut Chip) -> Result<Self, Error> {
-        let light_handles: Vec<LineHandle> = Self::LINES.iter()
-            .map(|&offset| {
-                chip.get_line(offset).unwrap()
-                    .request(LineRequestFlags::OUTPUT, 0, "peckboard")
-                    .unwrap()
-            }).collect();
-        let ir_handles: Vec<LineHandle> = Self::IR.iter()
-            .map(|&offset| {
-                chip.get_line(offset).unwrap()
-                    .request(LineRequestFlags::OUTPUT, 1, "peckboard_ir")
-                    .unwrap()
-            }).collect();
-        Ok(PeckLEDs{
-            light_handles,
-            ir_handles,
-        })
-    }
-}
-impl PeckKeys {
-    const INTERRUPT_CHIP: &'static str = "/dev/gpiochip2";
-    const INTERRUPT_LINE: u32 = 24;
-    const PECK_KEY_LINES: [u32; 3] = [13,14,15];
-
-    pub fn new(chip: &mut Chip) -> Result<Self, Error> {
-        Ok(PeckKeys{
-        })
-    }
-
-    pub async fn monitor(&mut self) -> Result<(), Error> {
-
+    pub async fn monitor(mut self) -> Result<(), Error> {
         tokio::spawn( async move {
             let mut chip2 = Chip::new(&Self::INTERRUPT_CHIP)
                 .map_err(|e:GpioError| Error::ChipError {source: e, chip: ChipNumber::Chip2})
@@ -92,7 +93,12 @@ impl PeckKeys {
 
             loop {
                 match events.next().await {
-                    Some(event) => { println!("Values are: {:?}", key_handles.get_values().unwrap()) },
+                    Some(event) => {
+                        let values = key_handles.get_values().unwrap();
+                        let position = values.iter().find(|&&x| x == 1).unwrap();
+                        println!("Position is {:?}", position);
+                        self.leds.pecked(position);
+                        println!("Values are: {:?}", key_handles.get_values().unwrap());},
                     None => break,
                 };
             }
@@ -100,6 +106,63 @@ impl PeckKeys {
         Ok(())
     }
 
+}
+impl PeckLEDs {
+    const RIGHT_LINES: [u32;3] = [0,3,6];
+    const CENTER_LINES: [u32;3] = [1,4,7];
+    const LEFT_LINES: [u32;3] = [2,5,8];
+
+    pub fn new(chip: &mut Chip) -> Result<Self, Error> {
+        let right_leds = chip.get_lines(&Self::RIGHT_LINES)
+            .map_err(|e:GpioError| Error::LinesGetError {source: e, lines: &Self::RIGHT_LINES}).unwrap()
+            .request(LineRequestFlags::OUTPUT, &LedState::Off.as_value(), "peck_leds")
+            .map_err(|e:GpioError| Error::LinesReqError {source: e, lines: &Self::RIGHT_LINES}).unwrap();
+        let center_leds = chip.get_lines(&Self::CENTER_LINES)
+            .map_err(|e:GpioError| Error::LinesGetError {source: e, lines: &Self::CENTER_LINES}).unwrap()
+            .request(LineRequestFlags::OUTPUT, &LedState::Off.as_value(), "peck_leds")
+            .map_err(|e:GpioError| Error::LinesReqError {source: e, lines: &Self::CENTER_LINES}).unwrap();
+        let left_leds = chip.get_lines(&Self::LEFT_LINES)
+            .map_err(|e:GpioError| Error::LinesGetError {source: e, lines: &Self::LEFT_LINES}).unwrap()
+            .request(LineRequestFlags::OUTPUT, &LedState::Off.as_value(), "peck_leds")
+            .map_err(|e:GpioError| Error::LinesReqError {source: e, lines: &Self::LEFT_LINES}).unwrap();
+        let peck_states: Vec<LedState> = vec![LedState::Off,LedState::Off,LedState::Off];
+
+        Ok(PeckLEDs{
+            right_leds,
+            center_leds,
+            left_leds,
+            peck_position: peck_states
+        })
+    }
+    pub fn pecked(&mut self, position: &u8) -> Result<(), Error> {
+        match position {
+            0 => {
+                let led_state = &self.peck_position[0].next().as_value();
+                self.right_leds.set_values(led_state).unwrap()},
+            1 => {
+                let led_state = &self.peck_position[1].next().as_value();
+                self.right_leds.set_values(led_state).unwrap()},
+            2 => {
+                let led_state = &self.peck_position[2].next().as_value();
+                self.right_leds.set_values(led_state).unwrap()},
+            _ => {println!("Invalid peck information")}
+        }
+        Ok(())
+    }
+}
+impl PeckKeys {
+
+    const IR: [u32; 3] = [9,10,11];
+    pub fn new(chip: &mut Chip) -> Result<Self, Error> {
+        let _ir_handles: Vec<LineHandle> = Self::IR.iter()
+            .map(|&offset| {
+                chip.get_line(offset).unwrap()
+                    .request(LineRequestFlags::OUTPUT, 1, "peckboard_ir")
+                    .unwrap()
+            }).collect();
+        Ok(PeckKeys{
+        })
+    }
 }
 
 #[derive(Debug)]
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() {
     let mut peck_board = PeckBoard::new("/dev/gpiochip4").await
         .expect("Couldn't initialize PeckBoard chip");
     println!("starting monitor");
-    peck_board.keys.monitor().await.unwrap();
+    peck_board.monitor().await.unwrap();
     loop {
         println!("A little tune");
         thread::sleep(Duration::from_secs(10000));
